84bda4283c428cf5ba9162a6349b689e9f0ef59a
91994 <SUDDALA.RAJUKUMAR@OUTLOOK.COM>
2022-02-09 22:35:47 +0530

    MONGODB

--- roboshop/components/mongodb.sh
+++ roboshop/components/mongodb.sh
@@ -3,5 +3,8 @@
 source components/common.sh
 
 Print "Downloading REPO"
+
+curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/roboshop-devops-project/mongodb/main/mongo.repo
+
 Start $?
 
