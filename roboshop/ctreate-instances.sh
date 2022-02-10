fc1fc3bc0ef792fa64de87b0d471fd03af684857
91994 <SUDDALA.RAJUKUMAR@OUTLOOK.COM>
2022-02-10 11:08:46 +0530

    AWS CLI

---
+++ roboshop/ctreate-instances.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+## To create Instance in command line we have to using this script.
+
+
+aws ec2 run-instances --image-id ami-0d997c5f64a74852c --instance-type t2.micro  --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$1}]"
