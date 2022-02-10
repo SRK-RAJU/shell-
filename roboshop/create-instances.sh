7036350993439d709f8f6d9ecd1f5072e1d5b2e5
91994 <SUDDALA.RAJUKUMAR@OUTLOOK.COM>
2022-02-10 12:19:14 +0530

    AWS CLI INSTANCES CREATION SCRIPT

--- roboshop/create-instances.sh
+++ roboshop/create-instances.sh
@@ -3,7 +3,7 @@
  COUNT=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=cart" | jq ".Reservations[].Instances[].InstanceId" | wc  -l)
 
 if [ $COUNT -eq 0 ]; then
-aws ec2 run-instances --image-id ami-0d997c5f64a74852c  --instance-type t2.micro --security-group-ids sg-0ff43ca729a6c7843  --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$1}]" | jq
+aws ec2 run-instances --image-id ami-0d997c5f64a74852c  --instance-type t2.micro  --security-group-ids sg-0ff43ca729a6c7843   --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$1}]" | jq
 
 else
   echo "Instance Already Exists"
