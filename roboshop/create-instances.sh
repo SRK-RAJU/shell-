f036ec8530ab4d783f39b152a0cf1069526e5c1a
91994 <SUDDALA.RAJUKUMAR@OUTLOOK.COM>
2022-02-10 12:51:51 +0530

    AWS CLI INSTANCES CREATION SCRIPT

--- roboshop/create-instances.sh
+++ roboshop/create-instances.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 ##  AWS CLI To create Instance in command line we have to using this script.
- COUNT=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=$1" | jq ".Reservations[].Instances[].InstanceId" | grep-v null  | wc  -l)
+ COUNT=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=$1" | jq ".Reservations[].Instances[].InstanceId" | grep -v null  | wc  -l)
 
 if [ $COUNT -eq 0 ]; then
 aws ec2 run-instances --image-id ami-0d997c5f64a74852c  --instance-type t2.micro  --security-group-ids sg-0ff43ca729a6c7843   --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$1}]" | jq
