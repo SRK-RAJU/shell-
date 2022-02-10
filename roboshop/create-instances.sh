c7a0b2e1889eb2a6d360262f7eba40b9a9a96b5d
91994 <SUDDALA.RAJUKUMAR@OUTLOOK.COM>
2022-02-11 00:20:34 +0530

    updating

--- roboshop/create-instances.sh
+++ roboshop/create-instances.sh
@@ -8,3 +8,8 @@ aws ec2 run-instances --image-id ami-0d997c5f64a74852c  --instance-type t2.micro
 else
   echo "Instance Already Exists"
   fi
+
+ IP=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=$1" | jq ".Reservations[].Instances[].PrivateIpAddress" | grep -v null)
+ ## xargs is used to remove the double quotes in displaying the output
+ sed -e "s/DNSNAME/$1.roboshop.internal/" -e "/s/IPADDRESS/${IP}" record.json >/tmp/record.json
+ aws route53 change-resource-record-sets --hosted-zone-id Z034785541G9EV6BV8GL --change-batch file:///tmp/record.json | jq
