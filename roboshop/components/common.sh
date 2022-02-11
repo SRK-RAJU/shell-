75d2771a947d69f2e4ba032986be762190139b43
91994 <SUDDALA.RAJUKUMAR@OUTLOOK.COM>
2022-02-11 21:50:57 +0530

    updating

--- roboshop/components/common.sh
+++ roboshop/components/common.sh
@@ -1,15 +1,15 @@
 Print ()
 {
-  echo -n -e "\e[1m$*\e[0m ... "
-  echo -e "\n\e[36m================= $* ================\e[0m" &>>$LOG
+  echo -n -e "\e[1m$?\e[0m ... "
+  echo -e "\n\e[36m================= $? ================\e[0m" &>>$LOG
 }
 Start() {
-  if [ "$*" -eq 0 ]; then
+  if [ $? -eq 0 ]; then
     echo -e "\e[1;32mSUCCESS\e[0m"
     else
       echo -e "\e[1;31mFAILURE\e[0m"
       echo -e "\e[1;33mScript failed and check the detailed log in $LOG file\e[0m]"
-      exit
+      exit 1
     fi
 }
 LOG=/tmp/roboshop.log
