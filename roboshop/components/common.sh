eb07c5f693420d381ba823c19ee6ee80f4f88771
91994 <SUDDALA.RAJUKUMAR@OUTLOOK.COM>
2022-02-11 22:20:13 +0530

    updating

--- roboshop/components/common.sh
+++ roboshop/components/common.sh
@@ -4,7 +4,7 @@ Print ()
   echo -e "\n\e[36m================= $1 ================\e[0m" &>>$LOG
 }
 Start() {
-  if [ $1 -eq 0 ]; then
+  if [ "$1" -eq 0 ]; then
     echo -e "\e[1;32mSUCCESS\e[0m"
     else
       echo -e "\e[1;31mFAILURE\e[0m"
