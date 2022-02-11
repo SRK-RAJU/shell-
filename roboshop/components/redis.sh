fd48cb8d6cca00192225630d6c1124372196d72a
91994 <SUDDALA.RAJUKUMAR@OUTLOOK.COM>
2022-02-12 00:14:16 +0530

    updating

--- roboshop/components/redis.sh
+++ roboshop/components/redis.sh
@@ -21,4 +21,4 @@ sed -i -e 's/127.0.0.1/0.0.0.0/'  /etc/redis.conf &>>$LOG
 Start $?
 
 Print "Start Redis Database"
-systemctl enable redis &>>$LOG && systemctl start redis &>>$LOG
+systemctl enable redis &>>$LOG && systemctl restart redis &>>$LOG
