380acc1125d96850526c93944a3ee86c7504a959
91994 <SUDDALA.RAJUKUMAR@OUTLOOK.COM>
2022-02-11 10:47:35 +0530

    updating

--- roboshop/create-instances.sh
+++ roboshop/create-instances.sh
@@ -13,5 +13,5 @@ else
 
  ## xargs is used to remove the double quotes in displaying the output
 
- sed -e "s/DNSNAME/$1.roboshop.internal/"  -e "s/IPADDRESS/${IP}" record.json >/tmp/record.json
+ sed -e "s/DNSNAME/$1.roboshop.internal/"  -e "s/IPADDRESS/${IP}/" record.json >/tmp/record.json
  aws route53 change-resource-record-sets --hosted-zone-id Z034785541G9EV6BV8GL --change-batch file:///tmp/record.json | jq
