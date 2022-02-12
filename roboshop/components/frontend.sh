b8d904abb3a77bc3443f65a95a7c4c38930cab60
91994 <SUDDALA.RAJUKUMAR@OUTLOOK.COM>
2022-02-12 21:40:45 +0530

    updating

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -4,7 +4,7 @@ source components/common.sh
 MSPACE=$(cat $0 | grep Print | awk -F '"' '{print $2}' | awk '{ print length }' | sort | tail -1)
 
 Print "Installing Nginx"
-yum install nginx -y &>>$LOG
+yum reinstall nginx -y &>>$LOG
 Stat $?
 
 Print " Downloading HTML Pages"
