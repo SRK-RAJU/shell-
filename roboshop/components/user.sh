adb6dbb1f28a20d2045bb7a22b31349e8e6f1832
91994 <SUDDALA.RAJUKUMAR@OUTLOOK.COM>
2022-02-12 12:53:05 +0530

    updating

--- roboshop/components/user.sh
+++ roboshop/components/user.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
 source components/common.sh
-MSPACE=$(cat $0 components/common.sh | grep ^Print | awk -F '"' '{print $2}' | awk '{ print length }' | sort | tail -1)
+MSPACE=$(cat $0  | grep ^Print | awk -F '"' '{print $2}' | awk '{ print length }' | sort | tail -1)
 Print "Installing Nodejs "
 yum install nodejs make gcc-c++ -y &>>$LOG
 Start $?
 
 ## As part of operating system standards, we run all the applications and databases as a normal user but not with root user.
 
-##o to run the catalogue service we choose to run as a normal user and that user name should be more relevant to the project. Hence we will use roboshop as the username to run the service.
 Print "Add Roboshop User"
 id roboshop &>>$LOG
 if [ $? -eq 0 ]; then
@@ -18,37 +17,37 @@ useradd roboshop &>>$LOG
 fi
 Start $?
 ##So let's switch to the roboshop user and run the following commands.
-Print "Download Catalogue"
-curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip" &>>$LOG
+Print "Download User"
+curl -s -L -o /tmp/user.zip "https://github.com/roboshop-devops-project/user/archive/main.zip" &>>$LOG
 Start $?
 Print "Remove the Roboshop Project"
-rm -rf /home/roboshop/catalogue
+rm -rf /home/roboshop/user
 Start $?
-Print "Extracting Catalogue"
-unzip -o -d /home/roboshop  /tmp/catalogue.zip &>>$LOG
+Print "Extracting user"
+unzip -o -d /home/roboshop  /tmp/user.zip &>>$LOG
 Start $?
 Print " Copy Content"
-mv /home/roboshop/catalogue-main  /home/roboshop/catalogue
+mv /home/roboshop/user-main  /home/roboshop/user
 Start $?
 Print "Install Node.js Dependencies"
-cd /home/roboshop/catalogue
+cd /home/roboshop/user
 ##npm install  --unsafe-perm &>>$LOG
 ##Start $?
 Print "Fix App Permission"
 sudo chown -R roboshop:roboshop  /home/roboshop
 Start $?
 Print "Update DNS Records in SystemID Config"
-sed -i -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/' /home/roboshop/catalogue/systemd.service &>>$LOG
+sed -i -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/' /home/roboshop/user/systemd.service &>>$LOG
 Start $?
 
 
 ##Print "Copy SystemID file"
-##mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service
-## mv  /home/roboshop/catalogue/systemd.service  /etc/systemd/system/catalogue.service  &>>$LOG
+##mv /home/roboshop/user/systemd.service /etc/systemd/system/user.service
+## mv  /home/roboshop/user/systemd.service  /etc/systemd/system/user.service  &>>$LOG
 
 Start "Finally Catalogue Service Started"
 
-systemctl daemon-reload &>>$LOG && systemctl restart catalogue &>>$LOG  && systemctl enable catalogue &>>$LOG
+systemctl daemon-reload &>>$LOG && systemctl restart user &>>$LOG  && systemctl enable user &>>$LOG
 
 Print "Checking DB connections from App"
 sleep 5
