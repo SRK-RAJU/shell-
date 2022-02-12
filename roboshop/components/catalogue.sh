1705e4b3ba666359a091663d0e20adfd13373194
91994 <SUDDALA.RAJUKUMAR@OUTLOOK.COM>
2022-02-12 12:00:48 +0530

    updating

--- roboshop/components/catalogue.sh
+++ roboshop/components/catalogue.sh
@@ -41,7 +41,7 @@ Print "Update DNS Records in SystemID Config"
 sed -i -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/' /home/roboshop/catalogue/systemd.service &>>$LOG
 Start $?
 Print "Copy SystemID file"
-mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service
+##mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service
 ## mv  /home/roboshop/catalogue/systemd.service  /etc/systemd/system/catalogue.service  &>>$LOG
 Start "Finally Catalogue Service Started"
 systemctl daemon-reload &>>$LOG && systemctl restart catalogue &>>$LOG  && systemctl enable catalogue &>>$LOG
