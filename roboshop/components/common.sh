0414bf2db0919b072826d38b67179041c4c1d0e5
91994 <SUDDALA.RAJUKUMAR@OUTLOOK.COM>
2022-02-12 12:57:12 +0530

    updating

--- roboshop/components/common.sh
+++ roboshop/components/common.sh
@@ -23,3 +23,38 @@ Start() {
 }
 LOG=/tmp/roboshop.log
 rm -f $LOG
+
+Print "Installing Nodejs "
+yum install nodejs make gcc-c++ -y &>>$LOG
+Start $?
+
+## As part of operating system standards, we run all the applications and databases as a normal user but not with root user.
+
+Print "Add Roboshop User"
+id roboshop &>>$LOG
+if [ $? -eq 0 ]; then
+echo User Roboshop already exists &>>$LOG
+else
+useradd roboshop &>>$LOG
+fi
+Start $?
+##So let's switch to the roboshop user and run the following commands.
+Print "Download User"
+curl -s -L -o /tmp/user.zip "https://github.com/roboshop-devops-project/user/archive/main.zip" &>>$LOG
+Start $?
+Print "Remove the Roboshop Project"
+rm -rf /home/roboshop/user
+Start $?
+Print "Extracting user"
+unzip -o -d /home/roboshop  /tmp/user.zip &>>$LOG
+Start $?
+Print " Copy Content"
+mv /home/roboshop/user-main  /home/roboshop/user
+Start $?
+Print "Install Node.js Dependencies"
+cd /home/roboshop/user
+##npm install  --unsafe-perm &>>$LOG
+##Start $?
+Print "Fix App Permission"
+sudo chown -R roboshop:roboshop  /home/roboshop
+Start $?
--- roboshop/components/user.sh
+++ roboshop/components/user.sh
@@ -2,40 +2,7 @@
 
 source components/common.sh
 MSPACE=$(cat $0  | grep ^Print | awk -F '"' '{print $2}' | awk '{ print length }' | sort | tail -1)
-Print "Installing Nodejs "
-yum install nodejs make gcc-c++ -y &>>$LOG
-Start $?
 
-## As part of operating system standards, we run all the applications and databases as a normal user but not with root user.
-
-Print "Add Roboshop User"
-id roboshop &>>$LOG
-if [ $? -eq 0 ]; then
-echo User Roboshop already exists &>>$LOG
-else
-useradd roboshop &>>$LOG
-fi
-Start $?
-##So let's switch to the roboshop user and run the following commands.
-Print "Download User"
-curl -s -L -o /tmp/user.zip "https://github.com/roboshop-devops-project/user/archive/main.zip" &>>$LOG
-Start $?
-Print "Remove the Roboshop Project"
-rm -rf /home/roboshop/user
-Start $?
-Print "Extracting user"
-unzip -o -d /home/roboshop  /tmp/user.zip &>>$LOG
-Start $?
-Print " Copy Content"
-mv /home/roboshop/user-main  /home/roboshop/user
-Start $?
-Print "Install Node.js Dependencies"
-cd /home/roboshop/user
-##npm install  --unsafe-perm &>>$LOG
-##Start $?
-Print "Fix App Permission"
-sudo chown -R roboshop:roboshop  /home/roboshop
-Start $?
 Print "Update DNS Records in SystemID Config"
 sed -i -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/' /home/roboshop/user/systemd.service &>>$LOG
 Start $?
