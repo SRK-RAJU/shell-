aca8065ea9ddf8ff851b4f710359ea453b2387db
91994 <SUDDALA.RAJUKUMAR@OUTLOOK.COM>
2022-02-12 21:38:00 +0530

    updating

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -22,15 +22,15 @@ Stat $?
 Print "Copying files to nginx path"
 mv /tmp/frontend-main/static/* /usr/share/nginx/html/. &>>$LOG
 Stat $?
-Print "Copy nginx roboshop into config file"
+Print "Copy nginx roboshop  config file"
 cp /tmp/frontend-main/localhost.conf /etc/nginx/default.d/roboshop.conf &>>$LOG
 Stat $?
 Print "Update Nginx Config file"
 sed -i -e '/catalogue/ s/localhost/catalogue.roboshop.internal/' -e '/cart/ s/localhost/cart.roboshop.internal/'  -e '/user/ s/localhost/user.roboshop.internal/'  -e '/payment/ s/localhost/payment.roboshop.internal/'  -e '/shipping/ s/localhost/shipping.roboshop.internal/'   /etc/nginx/default.d/roboshop.conf  &>>$LOG
 Stat $?
-Print "Enabling Nginx"
-systemctl enable nginx &>>$LOG
-Stat $?
 Print "ReStarting Nginx"
 systemctl restart nginx &>>$LOG
+Stat $?
+Print "Enabling Nginx"
+systemctl enable nginx &>>$LOG
 Stat $?
