9d14e3f9349075d96f6218f625342502c74e2eff
91994 <SUDDALA.RAJUKUMAR@OUTLOOK.COM>
2022-02-13 17:44:52 +0530

    updating

--- roboshop/components/redis.sh
+++ roboshop/components/redis.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 source components/common.sh
-MSPACE=$(cat $0 | grep ^Print | awk -F '"' '{print $2}' | awk '{ print length }' | sort | tail -1)
+MSPACE=$(cat $0 | grep ^Print | awk -F '"' '{print $2}' | awk '{ print length }' | sort | tail -1 )
+
 Print "Installing redis repos"
-yum install yum-utils  http://rpms.remirepo.net/enterprise/remi-release-7.rpm -y
+sudo yum install yum-utils  http://rpms.remirepo.net/enterprise/remi-release-7.rpm -y
 Stat $?
 Print "enable Redis Repos "
-yum-config-manager --enable remi &>>$LOG
+sudo yum-config-manager --enable remi &>>$LOG
 Stat $?
 Print "Install  Redis "
-yum install redis -y &>>$LOG
+sudo yum install redis -y &>>$LOG
 Stat $?
 Print "Update Redis Listen Address "
 sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/redis.conf /etc/redis/redis.conf &>>$LOG
