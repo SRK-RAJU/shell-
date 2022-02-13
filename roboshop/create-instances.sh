f5f6ed000db991403da0353602c6a6e45fe5b171
91994 <SUDDALA.RAJUKUMAR@OUTLOOK.COM>
2022-02-13 15:05:06 +0530

    updating

--- roboshop/create-instances.sh
+++ roboshop/create-instances.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 ##  AWS CLI To create Instance in command line we have to using this script.
- COUNT=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=$1" | jq ".Reservations[].Instances[].PrivateIpAddress" | grep -v null | wc  -l )
+COUNT=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=$1" | jq ".Reservations[].Instances[].PrivateIpAddress" | grep -v null | wc -1 )
 
 if [ $COUNT -eq 0 ]; then
-aws ec2 run-instances --image-id ami-0d997c5f64a74852c  --instance-type t2.micro  --security-group-ids sg-0ff43ca729a6c7843   --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$1}]" | jq
-
+aws ec2 run-instances --image-id ami-0d997c5f64a74852c  --instance-type t2.micro  --security-group-ids sg-0ff43ca729a6c7843   --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$1}]" | jq &>/dev/null
 else
-  echo "Instance Already Exists"
+  echo -e "\e[1;33m$1 Instance Already Exists\e[0m"
+  return
   fi
+  sleep 2
 
  IP=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=$1" | jq ".Reservations[].Instances[].PrivateIpAddress" | grep -v null  | xargs )
 
  ## xargs is used to remove the double quotes in displaying the output
 
- sed -e "s/DNSNAME/$1.roboshop.internal/"  -e "s/IPADDRESS/${IP}/" record.json>/tmp/record.json
+ sed -e "s/DNSNAME/$1.roboshop.internal/"  -e "s/IPADDRESS/${IP}/" record.json >/tmp/record.json
  aws route53 change-resource-record-sets --hosted-zone-id Z034785541G9EV6BV8GL  --change-batch  file:///tmp/record.json | jq
