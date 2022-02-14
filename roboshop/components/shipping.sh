043835607aa9402bcdc62fb75e0fa7388d7a8acc
91994 <SUDDALA.RAJUKUMAR@OUTLOOK.COM>
2022-02-14 21:36:08 +0530

    updating

--- roboshop/components/shipping.sh
+++ roboshop/components/shipping.sh
@@ -11,7 +11,7 @@ MAVEN
 Print "Checking DB Connections from App"
 sleep 15
 STAT=$(curl -s http://localhost:8080/health)
-if [ "$STAT" == "OK" ] ; then
+if [ "$STAT" == "OK" ]; then
   Stat 0
 else
   Stat 1
