d063bd49383150ff33d96a36bf88599f41c78b22
91994 <SUDDALA.RAJUKUMAR@OUTLOOK.COM>
2022-02-15 21:36:40 +0530

    updating

--- roboshop/components/shipping.sh
+++ roboshop/components/shipping.sh
@@ -7,12 +7,14 @@ COMPONENT_NAME=Shipping
 COMPONENT=shipping
 
 MAVEN
-CHECK_SHIPPING_FROM_APP
-##Print "Checking DB Connections from App"
-##sleep 15
-##STAT=$(curl -s "http://localhost:8080/health" | jq )
-##if [ "$STAT" == "OK" ]; then
- ## Stat 0
-##else
- ## Stat 1
-##fi
+##CHECK_SHIPPING_FROM_APP
+Print "Checking DB Connections from App"
+sleep 15
+echo status = $STAT
+
+STAT=$(curl -s "http://localhost:8080/health")
+if [ "$STAT" == "OK" ]; then
+  Stat 0
+else
+ Stat 1
+fi
